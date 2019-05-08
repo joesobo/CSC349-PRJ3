3de6bc2e23ce87bf67cac2226bafc551c850f0e8
Salman Wajahat <swajahat@calpoly.edu>
2019-05-08 10:36:37 -0700

    Update FactoryProblem.java
    link to the algorithm: http://www.nhu.edu.tw/~chun/DS(II)-Ch13-GreedyAlgorithm&DynamicProgram.pdf

--- FactoryProblem.java
+++ FactoryProblem.java
@@ -60,12 +60,59 @@ public class FactoryProblem{
             }
         }
 
-        factory(n, e1, e2, x1, x1, a, t);
+        factory(n, e1, e2, x1, x2, a, t);
         inputReader.close();
         reader.close();
     }
 
     public static void factory(int n, int e1, int e2, int x1, int x2, int[][]a, int[][]t){
+        int f1[] = new int [n];
+        int f2[] = new int [n];
+        int l[][] = new int[2][n];
+
+        f1[0] = e1 + a[0][0];
+        f1[0] = e2 + a[1][0];
+
+        for (int j = 2; j <= n; j++) {
+            if (f1[j-1] + a[0][j] <= f2[j-1] + t[1][j-1] + a[0][j]) {
+                f1[j] = f1[j-1] + a[0][j];
+                l[0][j] = 1;
+            }
+            else {
+                f1[j] = f2[j-1] + t[1][j-1] + a[0][j];
+                l[0][j] = 2;
+            }
+
+            if (f2[j-1] + a[1][j] <= f1[j-1] + t[0][j-1] + a[1][j]){
+                f2[j] = f2[j-1] + a[1][j];
+                l[1][j] = 2;
+            }
+            else {
+                f2[j] = f1[j-1] + t[0][j-1] + a[1][j];
+                l[1][j] = 1;
+            }
+        }
+         System.out.println("Fastest time is: " + minimum((f1[n] + x1), (f2[n] + x2)));
+         printLine(n,l);
+    }
+
+
+
+    private static int minimum(int a, int b) {
+        if ( a < b) {
+            return a;
+        }
+        else
+            return b;
+        }
+
+    private static void printLine(int n, int[][] l) {
+        int i = 1;
+        System.out.println("Station "+ i + ", line " + n);
+        for (int j=n; j>=2; j--){
+            i = l[i][j];
+            System.out.println("Station "+ i + ", line"  + (j-1));
+        }
 
     }
-}
+    }
